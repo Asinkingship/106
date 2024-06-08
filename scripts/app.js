10499b01b1bbcbc8f7aaf534d97fb70eefe756a4
Asinkingship <kbosborne1982@gmail.com>
2024-06-08 13:53:34 -0400

    class 1 work

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+function sayHello(){
+    console.log("Hello there")
+}
+
+function sayGoodbye(){
+    console.log("Bye Bye")
+}
+
+function init(){
+    console.log("hello world");
+    sayHello()
+}
+
+window.onload = init;
