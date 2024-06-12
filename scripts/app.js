885e02c324b11f0f0af7c04634067c7b178610c1
Asinkingship <kbosborne1982@gmail.com>
2024-06-11 22:49:44 -0400

    completed class 2, and playing with ai

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,69 @@
-function sayHello(){
-    console.log("Hello there")
-}
-
-function sayGoodbye(){
-    console.log("Bye Bye")
+function saveTask(){
+    console.log("Saving task");
 }
 
 function init(){
-    console.log("hello world");
-    sayHello()
-    sayGoodbye()
+    console.log("task manager")
+
+
+    //load data
+
+
+    //hook the events
+    $("#btnSave").click(saveTask);
 }
 
 
-//testing for change
 window.onload = init;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sayHello(){
+//     console.log("Hello there")
+// }
+
+// function sayGoodbye(){
+//     console.log("Bye Bye")
+// }
+
+// function init(){
+//     console.log("hello world");
+//     sayHello()
+//     sayGoodbye()
+// }
+
+
+// //testing for change
+// window.onload = init;
