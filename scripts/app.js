3dac8900e52e66ab78f7bd63c997c7c9912e30c5
Asinkingship <kbosborne1982@gmail.com>
2024-06-13 22:39:35 -0400

    update from session3

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,50 @@
 function saveTask(){
     console.log("Saving task");
+    //get the values
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    console.log(title, description, color, date, status, budget);
+
+    //build the object
+    let taskToSave = new Task(title, description, color, date, status, budget);
+    console.log(taskToSave);
+
+    //save to server
+
+
+
+    //display the info from the server
+    displayTask(taskToSave);
+
+    //try to creare a function that clears all the inputs at the moment of executong the save task
+    
+}
+
+function displayTask(task){
+
+    let syntax = 
+    `
+    <div class='task'>
+        <div class='info'>
+            <h3>${task.title}</h3>
+            <h5>${task.description}</h5>
+        </div>
+        <label class="status">${task.status}</label>
+        <div class="date-budget">
+            <label>${task.date}</label>
+            <label>${task.budget}</label>
+        </div>
+    </div>
+    `
+    $(".list-task").append(syntax);
+    //complete the rest of the inputs
+
+
+
 }
 
 function init(){
