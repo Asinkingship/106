dd475411a86791048e505ec5a0a4e9602cd78e5c
Asinkingship <kbosborne1982@gmail.com>
2024-06-15 15:04:48 -0400

    added session 4

--- scripts/app.js
+++ scripts/app.js
@@ -9,19 +9,69 @@ function saveTask(){
     const budget = $("#numBudget").val();
     console.log(title, description, color, date, status, budget);
 
+
+
     //build the object
     let taskToSave = new Task(title, description, color, date, status, budget);
     console.log(taskToSave);
 
     //save to server
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+
+        //here i need to create the logic to define that i what to send the taskToSave object
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        
+        //////////
+        success: function(response){
+            console.log(response);
+            displayTask(taskToSave);
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
+
+    //just render the messages that comes from your user
+
+
+
 
 
 
     //display the info from the server
-    displayTask(taskToSave);
 
     //try to creare a function that clears all the inputs at the moment of executong the save task
     
+}
+
+function loadTask(){
+    $.ajax({
+        type: "GET",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks",
+        success: function(response)
+        {
+            let data = JSON.parse(response);
+            for(let i=0; i<data.length; i++)
+            {
+                let task = data[i];  // get the object
+                if (task.name=="KB") // if the name is the same as the
+                {
+                    displayTask(task); //render it
+                }
+            }
+        },
+        error: function(error)
+        {
+            console.log(error);
+        }
+    });
+    //please fo this
+    //get the data from the http://fsdiapi.azurewebsites.net/api/tasks
+    //console.log the results
+
 }
 
 function displayTask(task){
@@ -43,8 +93,19 @@ function displayTask(task){
     $(".list-task").append(syntax);
     //complete the rest of the inputs
 
+}
 
-
+function testRequest(){
+    $.ajax({
+        type: "GET",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function(response){
+            console.log(response);
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
 }
 
 function init(){
@@ -52,8 +113,7 @@ function init(){
 
 
     //load data
-
-
+    loadTask()
     //hook the events
     $("#btnSave").click(saveTask);
 }
